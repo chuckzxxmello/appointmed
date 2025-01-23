195b846ba4471e3f43f5a18d0093fdd3f6119d42
Chuckz Espanola <main.chuckie.espanola@cvsu.edu.ph>
2025-01-24 00:23:32 +0800

    UI Major Updates

--- src/pages/auth/app.js
+++ src/pages/auth/app.js
@@ -206,3 +206,8 @@ const showError = (message) => {
         errorModal.hide();
     }, 5000);
 };
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
